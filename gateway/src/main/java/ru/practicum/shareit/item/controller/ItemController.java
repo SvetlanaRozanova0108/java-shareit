511166e4a439e8e4fb844d1d129ad1b617ba73a6
Svetlana Rozanova <rozanovasa@yandex.ru>
2024-12-23 14:41:24 +0300

    ShareIt-add-item-requests-and-gateway-v49

--- gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.client.ItemClient;
-import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.CommentSaveDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemSaveDto;
