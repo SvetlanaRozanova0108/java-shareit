e71074210fa5d3a679a7f78725c8a32ac289a7c3
Svetlana Rozanova <rozanovasa@yandex.ru>
2024-12-24 22:36:55 +0300

    ShareIt-add-item-requests-and-gateway-v57

--- server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.booking.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
